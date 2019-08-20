410ad04b59e157d81290198fdf1109512acc8b3c
Zachary I Friedman <zif@umd.edu>
2019-08-20 16:46:18 -0400

    Doing an initial commit for all the work up until this point: - created extension icons with ORA in white on a green circulare background and dark gray on a light gray background - created a shell of a google chrome extension that uses background.js to only run the css and js code changes when on a *.kuali.co/res or *.kuali.co/dashboard website - also in background.js has a system for allowing the used to enable/disable the plugin code and change the icon to green/gray based on the user clicking the extension icon - added css to hide all instances of the Baltimore only direct F&A fields throughout the award docs and time and money docs - added javascript to insert the current date into the notice date fields on new award docs and new t&m docs (will not overwrite existing dates) and attempts to hightlight those notice dates fields in light green when they are autopopulated (found this coloring may not always show)

---
+++ background.js
@@ -0,0 +1,151 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @filedescription Initializes the extension's background page.
+ */
+
+
+ /**
+  * executes the js code to update/override the CSS of the KR page
+  *
+  */
+
+
+
+ /**** toggle chrome extension on and off based on user clicking on extension icon ****/
+ /**** reference: https://stackoverflow.com/questions/16136275/how-to-make-on-off-buttons-icons-for-a-chrome-extension ***/
+ // start with the extension enabled
+ var extensionEnabled = true;
+
+ //add a listener that when the button is clicked toggles the extension off and the second time on, then off, etc
+ chrome.browserAction.onClicked.addListener(function(tab) {
+   extensionEnabled = !extensionEnabled;
+   updatePageIfExtensionEnabled(extensionEnabled, tab);
+ });
+
+
+ //add a listener that when going to any new page/refreshing page, etc, sets the ora icon to gray (then only for times when the plugin is actually active will it be turned to green by the next listener) - couldn't figure out another way to do it because the other listener only fires when the URL matches KR, otherwise nothing happens
+ chrome.webNavigation.onBeforeNavigate.addListener(function(tab) {
+   chrome.browserAction.setIcon({path: "ora_icon_off_128.png", tabId:tab.id});
+}, {});
+
+ //add a listener for each reload of the page (as the extension will need to run on each reload)
+ //but only run at all if it matches specific URLs (to keep it specific looking for kuali.co /res or /dashboard...later may decide to just have it match any kuali.co URL, it's a judgement call on how specific we want to be)
+ chrome.webNavigation.onCompleted.addListener(function(tab) {
+    //alert("webNavigation.onCompleted URL matched hostSuffix: kuali.co, pathContains: res");
+    updatePageIfExtensionEnabled(extensionEnabled, tab);
+}, {url: [{hostSuffix: "kuali.co", pathContains: "res"},{hostSuffix: "kuali.co", pathContains: "dashboard"}]});
+
+chrome.webNavigation.onCompleted.addListener(function(tab) {
+   //alert("webNavigation.onCompleted URL matched hostSuffix: kuali.co, pathContains: res");
+   updatePageIfExtensionEnabled(extensionEnabled, tab);
+}, {url: [{hostSuffix: "kuali.co", pathContains: "res"},{hostSuffix: "kuali.co", pathContains: "dashboard"}]});
+
+ /**
+  * actually loads the css and javascript to hide/update the page, if the exension is currently enabled
+  *
+  */
+  function updatePageIfExtensionEnabled(extensionEnabled, tab) {
+    if (extensionEnabled){
+      chrome.browserAction.setIcon({path: "ora_icon_128.png", tabId:tab.id});
+      chrome.tabs.insertCSS(tab.id, {file: "myStyles.css", allFrames: true});
+      chrome.tabs.executeScript(tab.id, {file: "contentScript.js", allFrames: true});
+      //  chrome.tabs.executeScript(tab.id, {code:"alert('on')"});
+    }
+    else{
+      chrome.browserAction.setIcon({path: "ora_icon_off_128.png", tabId:tab.id});
+      //chrome.tabs.executeScript(tab.id, {code:"alert('off')"});
+      //chrome.tabs.insertCSS(tab.id, {file: "empty.css", allFrames: true});
+    }
+}
+
+
+
+
+
+
+
+//var nav = new NavigationCollector();
+
+
+/*
+  chrome.webNavigation.onCompleted.addListener(function(data) {
+    //alert("alerting webNavigation.onCompleted for" + JSON.stringify(data));
+
+    //const retVal = updateCss();
+
+    if (typeof data)
+      console.log("data: " + JSON.stringify(data));
+    else
+      console.error("looks like an error, data: " + JSON.stringify(data));
+  });
+  */
+
+
+
+  // /**
+  //  * executes the js code to update/override the CSS of the KR page
+  //  *
+  //  */
+  // function updateCss() {
+  //   alert("inside updateCss()");
+  //
+  //   const querySelectorWiki = document.querySelector('#mp-topbanner > div > div:nth-child(1)');
+  //   if (querySelectorWiki) {
+  //     alert(`querySelectorWiki: ${querySelectorWiki.innerHTML}`);
+  //     querySelectorWiki.style.color = "green";
+  //   }
+  //
+  //   const querySelectorHead = document.querySelector('head');
+  //   if (querySelectorHead) {
+  //     alert(`querySelectorHead: ${querySelectorHead.innerHTML}`);
+  //   }
+  //
+  //   document.querySelector('body').style.backgroundColor = "purple";
+  //   document.body.style.backgroundColor = "purple";
+  //   document.bgColor = "orange";
+  //   alert("just tried to switch the background to purple, got here");
+  //
+  //
+  //
+  //
+  //
+  //   // var mainHeader = document.querySelector('#main-header');
+  //   // alert(`mainHeader: ${mainHeader.innerText}`);
+  //
+  //
+  //
+  //
+  //   // alert(`document: ${document.innerText}`);
+  //
+  //   // const footer = document.querySelector("#Uif-GlobalApplicationFooter");
+  //   // alert(`footer: ${footer.innerText}`);
+  //   // if (footer) {
+  //   //   footer.style.backgroundColor = "red";
+  //   // }
+  //   //
+  //   // const footerStyle2 = document.querySelector('div#footer-copyright');
+  //   // alert(`footerStyle2: ${JSON.stringify(footerStyle2)}`);
+  //   // if (footerStyle2) {
+  //   //   footerStyle2.style.backgroundColor = "red";
+  //   // }
+  //   //
+  //   //
+  //   //
+  //   // const anticipatedAmtRow = document.querySelector("#tab-DetailsDates\\:TimeMoney-div > table > tbody > tr:nth-child(5)");
+  //   // alert(`anticipatedAmtRow: ${JSON.stringify(anticipatedAmtRow)}`);
+  //   // if (anticipatedAmtRow) {
+  //   //   anticipatedAmtRow.style.visibility = "hidden";
+  //   // }
+  //   // //document.querySelector("#tab-DetailsDates\\:TimeMoney-div > table > tbody > tr:nth-child(5)").style.visibility = "hidden";
+  //   // document.body.style.backgroundColor = "lightgray";
+  //
+  // }
+
+// Reset the navigation state on startup. We only want to collect data within a
+// session.
+// chrome.runtime.onStartup.addListener(function() {
+//   nav.resetDataStorage();
+// });
