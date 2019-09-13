b0a6a5a74b3c502a704184a14baf41210ba87e3b
fuzzy-slippers <zif@umd.edu>
2019-09-12 23:05:42 -0400

    Working skeleton of messages indicating which KR module/page currently on using alerts for now - A number of changes leading to this - was writing out an 8 part git message but my computer crashed and I lost it, commiting again

---
+++ awdModule/awardTabContentScript.js
@@ -0,0 +1,48 @@
+//before trying to manipulate any of the form field values, etc make sure this page has a form named KualiForm
+if (document.forms["KualiForm"]) {
+
+  /* Award Document Section */
+
+  //double check that you are on a page that has the award doc notice date (award doc edit page)
+  if (document.forms["KualiForm"]["document.awardList[0].noticeDate"]) {
+    //make sure value is empty before pre-populating, we don't want to overwrite data the person just entered (and switched tabs)
+    if (!document.forms["KualiForm"]["document.awardList[0].noticeDate"].value) {
+      //set/prepopulate empty notice dates to the current date in "12/1/2019" format used by Kuali, which seems to match locale date string en-US, specifying in case someone has different settings in their browser, I believe Kuali will still require that string format
+      document.forms["KualiForm"]["document.awardList[0].noticeDate"].value = new Date().toLocaleDateString("en-US");
+      //also set the background color of the input field to a light green (similar to chromes autofill light blue) to indicate the field had been auto-filled just now (on next page refresh should go away)
+      document.forms["KualiForm"]["document.awardList[0].noticeDate"].style.backgroundColor = "#EBFFF7";
+    }
+  }
+
+
+  /* Time and Money Document Section */
+
+  //double check that you are on a page that has the t&m doc notice date (t&m doc in edit mode)
+  if (document.forms["KualiForm"]["document.awardAmountTransactions[0].noticeDate"]) {
+    //make sure value is empty before pre-populating, we don't want to overwrite data the person just entered
+    if (!document.forms["KualiForm"]["document.awardAmountTransactions[0].noticeDate"].value) {
+      //set/prepopulate empty notice dates to the current date in "12/1/2019" format used by Kuali, which seems to match locale date string en-US, specifying in case someone has different settings in their browser, I believe Kuali will still require that string format
+      document.forms["KualiForm"]["document.awardAmountTransactions[0].noticeDate"].value = new Date().toLocaleDateString("en-US");
+      //also set the background color of the input field to a light green (similar to chromes autofill light blue) to indicate the field had been auto-filled just now (on next page refresh should go away)
+      document.forms["KualiForm"]["document.awardAmountTransactions[0].noticeDate"].style.backgroundColor = "#EBFFF7";    }
+  }
+
+  //alert("document.forms[`KualiForm`] detected");
+  /* PHASE 2 - try to auto-enter zero dollars into transactions in time and money - needs more testing
+  if (document.forms["KualiForm"]["transactionBean.newPendingTransaction.obligatedAmount"])
+    document.forms["KualiForm"]["transactionBean.newPendingTransaction.obligatedAmount"].value = "0";
+  */
+}
+
+
+// alert("ON - alert from contentscript.js");
+// document.body.style.backgroundColor = "purple";
+
+// const obligatedAmountSelector = document.querySelector("#transactionBean\\.newPendingTransaction\\.obligatedAmount");
+// if (obligatedAmountSelector) {
+//   obligatedAmountSelector.value = "7";
+// }
+// const explanationSelector = document.querySelector('#document\2e documentHeader\2e explanation');
+// if (explanationSelector) {
+//   explanationSelector.value = 'text added by KR chrome browser extension';
+// }
--- background.js
+++ background.js
@@ -21,6 +21,7 @@
    extensionEnabled = !extensionEnabled;
    //default the icon to disabled or inactive depending on whether the extension is currenly on or off (later if the page one of the ones to be modified, the icon will be changed to enabled)
    setIconInactiveOrDisabled(extensionEnabled, tab);
+   //start the process to determine if the extension should be turned on (make sure extension is enabled + that we are on a KR page/tab that it should be turned on)
    updatePageIfExtensionEnabled(extensionEnabled, tab);
  });
 
@@ -33,32 +34,57 @@
  //add a listener for each reload of the page (as the extension will need to run on each reload)
  //but only run at all if it matches specific URLs (to keep it specific looking for kuali.co /res or /dashboard...later may decide to just have it match any kuali.co URL, it's a judgement call on how specific we want to be)
  chrome.webNavigation.onCompleted.addListener(function(tab) {
-   //now that we know we are on a KR page, call the content script that will detect the KR module and tab selected by checking the KualiForm action URL/value (will have to be passed back as a message)
-   chrome.tabs.executeScript(tab.id, {file: "detectActiveKRModuleTabContentScript.js", allFrames: true});
-
-   //test code
-   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-     chrome.tabs.sendMessage(tabs[0].id, {initialMessage: "sendActionPlease"}, function(response) {
-       alert(JSON.stringify(response));
-     });
-   });
-
-   //alert("webNavigation.onCompleted URL matched hostSuffix: kuali.co, pathContains: res");
+   //start the process to determine if the extension should be turned on (make sure extension is enabled + that we are on a KR page/tab that it should be turned on)
    updatePageIfExtensionEnabled(extensionEnabled, tab);
+   //console.log("webNavigation.onCompleted URL matched hostSuffix: kuali.co, pathContains: res");
 }, {url: [{hostSuffix: "kuali.co", pathContains: "res"},{hostSuffix: "kuali.co", pathContains: "dashboard"}]});
 
   // listen for messages from content scripts
+  // the message
   // currently we are listening for a specific "theFormAction" property on the request that should be coming from the detectActiveKRModuleTabContentScript.js script
   // it should be simpy sending along the KualiForm <form> action property/url on the currently loaded page, so we can figure out which KR module/tab such as the Award Module Special Review tab is currently loaded in the active tab in the browser
   // we will need this info to decide whether to have the extension update the current KR page or now (if its one of the KR pages we have decided to change/update)
   chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    alert(`request.theFormAction is: ${request.theFormAction}`)
+    console.log(`chrome.runtime.onMessage.addListener returned some message`);
+    //check the type of message received based on the response property - also make sure a valid form action URL was sent to us (not a blank string/null) before proceeding
+    if (request.theFormAction) {
+      const formActionFromMsg = request.theFormAction;
+      //determine the currently active tab info/object (since this is a listener, it doesn't have this info)
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        console.log(`iside the query function to get current tab: ${JSON.stringify(tabs)}`);
+        //make sure it returned something when querying for the current active tab
+        if (tabs[0]) {
+          const currentTab = tabs[0];
+          //console.log(`currentTab: ${JSON.stringify(currentTab)});
+
+          console.log(`request.theFormAction is: ${request.theFormAction} - we put it in the variable formActionFromMsg: ${formActionFromMsg}`);
+          //now that we know the current tab and the form action URL, pass these along to a function to check if its one of the valid KR modules/tabs for the extension to auto-activate
+          checkIfCurrentPageInListOfKRModulesTabs(currentTab, formActionFromMsg);
+
+        }
+      });
+
+    }
+
+
   });
 
 /*
  * ::Functions::
  */
 
+ /**
+  * helps determine which module/tab in KR the user is currently viewing in the active browser tab in Google chrome
+  * due to some limitations of background.js only being able to figure out the URL but not the content/DOM/form elements of the active tab in the browser
+  * we need to do this in 2 parts...this function is part one and kicks off the sequence of events
+  * This function does part 1) it loads the special content script we created that just reads the HTML <form> element with the name "KualiForm" of the current page and sends back the URL of the "action" property of that form (with the flag set for the content script to allow it access)
+  * then for the second part of actually doing something with action property which tells us the current KR module/tab being viewed, see the _ listener function above which is fired when we actually get the information back in the form of a message sent from the content script, which at that point things can keep going and we can use that info to decide if we turn the extension on based on the current KR page/tab
+  */
+  function initiateMessagePassingToFigureOutWhichKRModuleTabWeAreOn(tab) {
+    console.log(`calledinitiateMessagePassingToFigureOutWhichKRModuleTabWeAreOn`);
+    //now that we know we are on a KR page, call the content script that will detect the KR module and tab selected by checking the KualiForm action URL/value (will have to be passed back as a message)
+    chrome.tabs.executeScript(tab.id, {file: "detectActiveKRModuleTabContentScript.js", allFrames: true});
+  }
 
  /**
   * based on whether the extension is currently enabled (using flag), either set to the inactive color if clicked on or the disabled color if clicked off (turning it green will happen at a later stage right along with the code to actually modify the page, if thats the case)
@@ -73,13 +99,17 @@
   }
 
  /**
-  * check if the extension is currently on (using global flag keeping history of on/off), if so set to inactive icon initially and have it further check if it's a module/tab that should have changes made to the page, otherwise if off set the icon to the disabled color
+  * check if the extension is currently on (using global flag keeping history of on/off),
+  * if so set to inactive icon initially
+  * and have it further check if it's a module/tab that should have changes made to the page (if not will still show disabled)
+  * if extension determined to be clicked off by the user, dont need to do anything as the icon shows the off colors by default until it is explicitly turned on based on the KR module, etc
   */
   function updatePageIfExtensionEnabled(extensionEnabled, tab) {
-  if (extensionEnabled){
-    detectKRModuleUpdateAccordingly(tab);
+    if (extensionEnabled){
+      console.log(`called updatePageIfExtensionEnabled and determined extensionEnabled=true`);
+      initiateMessagePassingToFigureOutWhichKRModuleTabWeAreOn(tab);
+    }
   }
-}
 
 
 
@@ -87,7 +117,7 @@
  * change the extension icon to green to indicate its active on the current page
  */
  function setExtensionIconActiveColor(tab) {
-   alert(`about to set icon to green (inside setExtensionIconActiveColor)`);
+   console.log(`about to set icon to green (inside setExtensionIconActiveColor)`);
    chrome.browserAction.setIcon({path: "ora_icon_128.png", tabId:tab.id});
  }
 
@@ -106,54 +136,68 @@
    }
 
 
+
   /**
   * check if the page being loaded is one that is in the list of KR pages we plan to modify
   * (will allow us at the above level to skip trying to load any changes/code if we know we dont plan to modify this module/tab/page in KR)
   *
   */
-  function detectKRModuleUpdateAccordingly(tab) {
+  function checkIfCurrentPageInListOfKRModulesTabs(tab, actionStr) {
+    console.log(`passed to checkIfCurrentPageInListOfKRModulesTabs, tab.id: ${tab.id} and actionStr: ${actionStr}`);
+    const krAwardModuleAwardTabRegex = /\/awardHome.do/;
 
-    //make sure the tab.url is not undefined before using it to check which module the page is on
-    /* wont be using tab.URL
-    if (tab.url) {
-      if (/\/award/.test(tab.url)) {
-        alert(`detected this page has "/award" in the URL which is: ${tab.url} `);
-        awdModuleDetectTabAndUpdateAccordingly(tab);
-      }
-      else if (/\/time/.test(tab.url)) {
-            //TODO: Need to separate out the Time and Money CSS and Javascript into separate files and a function to detect the tab chosen needs to be created so its only imported on the one tab that needs it to run
-            alert(`detected this page has "\time" in the URL which is: ${tab.url} `);
-      }
-      //institutional
-      else if (/\/institutional/.test(tab.url)) {
-            alert(`detected this page has "\institutional" in the URL which is: ${tab.url} `);
-      }
-      else if (/\/proposalDevelopment/.test(tab.url)) {
-            alert(`detected this page has "\proposalDevelopment" in the URL which is: ${tab.url} `);
-      }
-      else if (/\/sub/.test(tab.url)) {
-            alert(`detected this page has "\sub" in the URL which is: ${tab.url} `);
-      }
-      else {
-        alert(`did not detect anything, but tab.url is: ${tab.url}`);
-      }
+    if (krAwardModuleAwardTabRegex.test(actionStr)) {
+      alert(`determined we are on the award tab of the KR award module...`);
+      makeKRAwardModuleAwardTabRelatedChanges(tab);
     }
-    */
   }
 
 
+  /**
+  * makes the customizations specific to the award module as well as change the icon to indicate the extension is actively making changes
+  * overlay the css and run the custom javascript to modify the current page (if the form elements to change, etc are present)
+  *
+  */
+  function makeKRAwardModuleAwardTabRelatedChanges(tab) {
+     //TODO: determine tab inside award module and based on that call function to update icon color and css/js based on tab selected (do nothing including leave icon yellow for tabs that do not have any custom overlays)
+     setExtensionIconActiveColor(tab);
+     chrome.tabs.insertCSS(tab.id, {file: "/awdModule/awardTabStyles.css", allFrames: true});
+     chrome.tabs.executeScript(tab.id, {file: "/awdModule/awardTabContentScript.js", allFrames: true});
+  }
 
-    /**
-    * makes the customizations specific to the award module as well as change the icon to indicate the extension is actively making changes
-    * overlay the css and run the custom javascript to modify the current page (if the form elements to change, etc are present)
-    *
-    */
-    function awdModuleDetectTabAndUpdateAccordingly(tab) {
-       //TODO: determine tab inside award module and based on that call function to update icon color and css/js based on tab selected (do nothing including leave icon yellow for tabs that do not have any custom overlays)
-       setExtensionIconActiveColor(tab);
-       chrome.tabs.insertCSS(tab.id, {file: "myStyles.css", allFrames: true});
-       chrome.tabs.executeScript(tab.id, {file: "contentScript.js", allFrames: true});
-    }
+
+    // make sure the tab.url is not undefined before using it to check which module the page is on
+    //
+    // /* wont be using tab.URL
+    // if (tab.url) {
+    //   if (/\/award/.test(tab.url)) {
+    //     console.log(`detected this page has /award in the URL which is: ${tab.url} `);
+    //     awdModuleDetectTabAndUpdateAccordingly(tab);
+    //   }
+    //   else if (/\/time/.test(tab.url)) {
+    //         //TODO: Need to separate out the Time and Money CSS and Javascript into separate files and a function to detect the tab chosen needs to be created so its only imported on the one tab that needs it to run
+    //         console.log(`detected this page has "\time" in the URL which is: ${tab.url} `);
+    //   }
+    //   //institutional
+    //   else if (/\/institutional/.test(tab.url)) {
+    //         console.log(`detected this page has "\institutional" in the URL which is: ${tab.url} `);
+    //   }
+    //   else if (/\/proposalDevelopment/.test(tab.url)) {
+    //         console.log(`detected this page has "\proposalDevelopment" in the URL which is: ${tab.url} `);
+    //   }
+    //   else if (/\/sub/.test(tab.url)) {
+    //         console.log(`detected this page has "\sub" in the URL which is: ${tab.url} `);
+    //   }
+    //   else {
+    //     console.log(`did not detect anything, but tab.url is: ${tab.url}`);
+    //   }
+    // }
+    // */
+
+
+
+
+
 
 
 
@@ -164,7 +208,7 @@
 
 /*
   chrome.webNavigation.onCompleted.addListener(function(data) {
-    //alert("alerting webNavigation.onCompleted for" + JSON.stringify(data));
+    //console.log("alerting webNavigation.onCompleted for" + JSON.stringify(data));
 
     //const retVal = updateCss();
 
@@ -182,44 +226,44 @@
   //  *
   //  */
   // function updateCss() {
-  //   alert("inside updateCss()");
+  //   console.log("inside updateCss()");
   //
   //   const querySelectorWiki = document.querySelector('#mp-topbanner > div > div:nth-child(1)');
   //   if (querySelectorWiki) {
-  //     alert(`querySelectorWiki: ${querySelectorWiki.innerHTML}`);
+  //     console.log(`querySelectorWiki: ${querySelectorWiki.innerHTML}`);
   //     querySelectorWiki.style.color = "green";
   //   }
   //
   //   const querySelectorHead = document.querySelector('head');
   //   if (querySelectorHead) {
-  //     alert(`querySelectorHead: ${querySelectorHead.innerHTML}`);
+  //     console.log(`querySelectorHead: ${querySelectorHead.innerHTML}`);
   //   }
   //
   //   document.querySelector('body').style.backgroundColor = "purple";
   //   document.body.style.backgroundColor = "purple";
   //   document.bgColor = "orange";
-  //   alert("just tried to switch the background to purple, got here");
+  //   console.log("just tried to switch the background to purple, got here");
   //
   //
   //
   //
   //
   //   // var mainHeader = document.querySelector('#main-header');
-  //   // alert(`mainHeader: ${mainHeader.innerText}`);
+  //   // console.log(`mainHeader: ${mainHeader.innerText}`);
   //
   //
   //
   //
-  //   // alert(`document: ${document.innerText}`);
+  //   // console.log(`document: ${document.innerText}`);
   //
   //   // const footer = document.querySelector("#Uif-GlobalApplicationFooter");
-  //   // alert(`footer: ${footer.innerText}`);
+  //   // console.log(`footer: ${footer.innerText}`);
   //   // if (footer) {
   //   //   footer.style.backgroundColor = "red";
   //   // }
   //   //
   //   // const footerStyle2 = document.querySelector('div#footer-copyright');
-  //   // alert(`footerStyle2: ${JSON.stringify(footerStyle2)}`);
+  //   // console.log(`footerStyle2: ${JSON.stringify(footerStyle2)}`);
   //   // if (footerStyle2) {
   //   //   footerStyle2.style.backgroundColor = "red";
   //   // }
@@ -227,7 +271,7 @@
   //   //
   //   //
   //   // const anticipatedAmtRow = document.querySelector("#tab-DetailsDates\\:TimeMoney-div > table > tbody > tr:nth-child(5)");
-  //   // alert(`anticipatedAmtRow: ${JSON.stringify(anticipatedAmtRow)}`);
+  //   // console.log(`anticipatedAmtRow: ${JSON.stringify(anticipatedAmtRow)}`);
   //   // if (anticipatedAmtRow) {
   //   //   anticipatedAmtRow.style.visibility = "hidden";
   //   // }
