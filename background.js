a062617688b54fb683f06a55fa54c2699eccfdfa
fuzzy-slippers <zif@umd.edu>
2019-09-20 22:55:40 -0400

    figured out issues with inconsistent icon color changes & refreshing on tab change this involved multiple initiatives (probably should have broken these into separate commits. They were: 1) figuring out that the icon changes behaved more consistently when not passing in a tab id at all, it turned out that the tab ids are optional and it just seems to be more more consistent when avoiding passing anything in, the default is the current window/tab 2) in order to get code to trigger when you switch between existing tabs rather than just opening a new tab/URL, had to utilize the chrome.tabs.onActivated.addListener listener which worked without the tab permission, but in testing it out determined that we would need the tab permission because we want to not only set the tab to disabled but also check if the tab switched to is a KR page that should show an enabled icon, so in order to do that, we needed to check the URL of the current page using the query tab function for the current tab and we wanted to filter on just when it was a *.kuali.co* page and in order to match against URLs, you had to have the manifest tab permission enabled (unfortunately, always try to keep permissions requested to a minimum) 3) also determined that a chrome.tabs.onUpdated.addListener could do the same thing as the webnavigation listener I had set up to check if a new page/URL was loaded. Basically, this was identical to the code neeeded for the switching tab listener (first set the icon to either yellow or dark gray and then run through the code to detremine if it is a KR page we want to enable the plugin for and if the extension is currently disabled/clicked off 4) broke out the code that was identical between the new change tab listener and the load new page listener into its own standalone function for easier maintenance (looks up the current tab by the URL pattern passed in for KR and runs the checking function) 5) tested out if I could disable the two listeners we had been previously been usingchrome.webNavigation.onBeforeNavigate.addListener on all URLs and chrome.webNavigation.onBeforeNavigate.addListener filtered by {url: [{hostSuffix: "kuali.co", pathContains: "res"},{hostSuffix: "kuali.co", pathContains: "dashboard"}]} so far in my testing it worked to disable both AND remove the webnavigation permission, so that should hopefully remove the warning that the extension can see peoples browsing history...but I'm not sure what the tab permission in it's place will result in with the warnings when installing for the user

--- background.js
+++ background.js
@@ -8,7 +8,7 @@
 
 
 
- /**** toggle chrome extension on and off based on user clicking on extension icon ****/
+ /**** toggle chrome extension on and off based on user clicking on extension icon with global background.js var ****/
  /**** reference: https://stackoverflow.com/questions/16136275/how-to-make-on-off-buttons-icons-for-a-chrome-extension ***/
  // start with the extension enabled
  var extensionEnabled = true;
@@ -19,26 +19,28 @@
 
  //add a listener that when the button is clicked toggles the extension off and the second time on, then off, etc
  chrome.browserAction.onClicked.addListener(function(tab) {
+   console.log(`click on icon detected via chrome.browserAction.onClicked.addListener`);
    extensionEnabled = !extensionEnabled;
    //default the icon to disabled or inactive depending on whether the extension is currenly on or off (later if the page one of the ones to be modified, the icon will be changed to enabled)
-   setIconInactiveOrDisabled(extensionEnabled, tab);
+   setIconInactiveOrDisabled(extensionEnabled);
    //start the process to determine if the extension should be turned on (make sure extension is enabled + that we are on a KR page/tab that it should be turned on)
    updatePageIfExtensionEnabled(extensionEnabled, tab);
  });
 
 
- //add a listener that when going to any new page/refreshing page, etc, if the extension is currently clicked on, default the icon to the inactive color but if it's clicked off default the icon to the disabled color
- chrome.webNavigation.onBeforeNavigate.addListener(function(tab) {
-   setIconInactiveOrDisabled(extensionEnabled, tab)
-}, {});
-
- //add a listener for each reload of the page (as the extension will need to run on each reload)
- //but only run at all if it matches specific URLs (to keep it specific looking for kuali.co /res or /dashboard...later may decide to just have it match any kuali.co URL, it's a judgement call on how specific we want to be)
- chrome.webNavigation.onCompleted.addListener(function(tab) {
-   //start the process to determine if the extension should be turned on (make sure extension is enabled + that we are on a KR page/tab that it should be turned on)
-   updatePageIfExtensionEnabled(extensionEnabled, tab);
-   //console.log("webNavigation.onCompleted URL matched hostSuffix: kuali.co, pathContains: res");
-}, {url: [{hostSuffix: "kuali.co", pathContains: "res"},{hostSuffix: "kuali.co", pathContains: "dashboard"}]});
+//  //add a listener that when going to any new page/refreshing page, etc, if the extension is currently clicked on, default the icon to the inactive color but if it's clicked off default the icon to the disabled color
+//  chrome.webNavigation.onBeforeNavigate.addListener(function(tab) {
+//    console.log(`detected new page/refresh via chrome.webNavigation.onBeforeNavigate.addListener, with a tab object that shows: ${JSON.stringify(tab)}`);
+//    setIconInactiveOrDisabled(extensionEnabled);
+// }, {});
+//
+//  //add a listener for each reload of the page (as the extension will need to run on each reload)
+//  //but only run at all if it matches specific URLs (to keep it specific looking for kuali.co /res or /dashboard...later may decide to just have it match any kuali.co URL, it's a judgement call on how specific we want to be)
+//  chrome.webNavigation.onCompleted.addListener(function(tab) {
+//    //start the process to determine if the extension should be turned on (make sure extension is enabled + that we are on a KR page/tab that it should be turned on)
+//    updatePageIfExtensionEnabled(extensionEnabled, tab);
+//    //console.log("webNavigation.onCompleted URL matched hostSuffix: kuali.co, pathContains: res");
+// }, {url: [{hostSuffix: "kuali.co", pathContains: "res"},{hostSuffix: "kuali.co", pathContains: "dashboard"}]});
 
   // listen for messages from content scripts
   // the message
@@ -51,8 +53,14 @@
     if (request.theFormAction) {
       const formActionFromMsg = request.theFormAction;
       //determine the currently active tab info/object (since this is a listener, it doesn't have this info)
-      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        console.log(`iside the query function to get current tab: ${JSON.stringify(tabs)}`);
+      chrome.tabs.query({
+      "active":        true,
+      "currentWindow": true,
+      "status":        "complete",
+      "windowType":    "normal",
+      "url": "https://*.kuali.co/*"
+      }, function(tabs) {
+        console.log(`inside the query function to get current tab: ${JSON.stringify(tabs)}`);
         //make sure it returned something when querying for the current active tab
         if (tabs[0]) {
           const currentTab = tabs[0];
@@ -66,9 +74,40 @@
     }
   });
 
+  // switch tabs - may posssibly use ['*://*/*foo.bar', '*://*/*foo.bar?*'] see: https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Match_patterns
+  chrome.tabs.onActivated.addListener(function(activeInfo) {
+      //alert(`chrome.tabs.onActivated listener triggered for: ${JSON.stringify(activeInfo)}, so setting icon yellow/gray first`);
+      setIconInactiveOrDisabled(extensionEnabled);
+      updateCurrentTabIfKRAndExtensionEnabled(extensionEnabled, "https://*.kuali.co/*");
+  });
+
+  // new URL loaded in either new or existing tab
+  chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+      //alert(`chrome.tabs.onActivated listener triggered for: ${JSON.stringify(activeInfo)}, so setting icon yellow/gray first`);
+      setIconInactiveOrDisabled(extensionEnabled);
+      updateCurrentTabIfKRAndExtensionEnabled(extensionEnabled, "https://*.kuali.co/*");
+  });
+
 /*
  * ::Functions::
  */
+
+function updateCurrentTabIfKRAndExtensionEnabled(extensionEnabled, urlPatterns) {
+  chrome.tabs.query({
+  "active":        true,
+  "currentWindow": true,
+  "status":        "complete",
+  "windowType":    "normal",
+  "url": urlPatterns
+  }, function(tabs) {
+   //if the url pattern doesnt match KR as specified in the url property above, this function will run but with an empty array, so don't do anything for these pages - this makes sure it returned something when querying for the current active tab
+   if (tabs && tabs[0]) {
+     const currentTab = tabs[0];
+     //alert(`about to call updatePageIfExtensionEnabled passing in currentTab: ${JSON.stringify(currentTab)}`)
+     updatePageIfExtensionEnabled(extensionEnabled, currentTab);
+   }
+  });
+}
 
  /**
   * helps determine which module/tab in KR the user is currently viewing in the active browser tab in Google chrome
@@ -86,12 +125,14 @@
  /**
   * based on whether the extension is currently enabled (using flag), either set to the inactive color if clicked on or the disabled color if clicked off (turning it green will happen at a later stage right along with the code to actually modify the page, if thats the case)
   */
-  function setIconInactiveOrDisabled(extensionEnabled, tab) {
+  function setIconInactiveOrDisabled(extensionEnabled) {
     if (extensionEnabled){
-      setExtensionIconInactiveColor(tab);
+      console.log(`setIconInactiveOrDisabled called and extensionEnabled=true so setting icon color to yellow via call to setExtensionIconInactiveColor func`);
+      setExtensionIconInactiveColor();
     }
     else{
-      setExtensionIconDisabledColor(tab);
+      //alert(`extensionEnabled=false so setting icon color to dark gray`);
+      setExtensionIconDisabledColor();
     }
   }
 
@@ -113,25 +154,53 @@
 /*
  * change the extension icon to green to indicate its active on the current page
  */
- function setExtensionIconActiveColor(tab) {
-   console.log(`about to set icon to green (inside setExtensionIconActiveColor) using getTabId(tab) which is: ${getTabId(tab)}`);
-   chrome.browserAction.setIcon({path: "ora_icon_128.png", tabId:getTabId(tab)});
+ function setExtensionIconActiveColor() {
+   //console.log(`about to set icon to green (inside setExtensionIconActiveColor) using getTabId(tab) which is: ${getTabId(tab)}`);
+   chrome.browserAction.setIcon({path: "ora_icon_128.png"});
  }
 
  /*
   * change the extension icon to yellow to indicate we are on a KR page, but it's not doing anything to the current page
   */
-  function setExtensionIconInactiveColor(tab) {
-    console.log(`inside setExtensionIconInactiveColor the tab passed in shows: ${JSON.stringify(tab)} and also the getTabId(tab) being used is showing: ${getTabId(tab)}`);
+  function setExtensionIconInactiveColor() {
+    //console.log(`inside setExtensionIconInactiveColor the tab passed in shows: ${JSON.stringify(tab)} and also the getTabId(tab) being used is showing: ${getTabId(tab)}`);
     console.log(`about to set icon to yellow (inside setExtensionIconInactiveColor)`);
-    chrome.browserAction.setIcon({path: "ora_icon_off_yellow_128.png", tabId:getTabId(tab)});
+    chrome.browserAction.setIcon({path: "ora_icon_off_yellow_128.png"});
+    // we were running into issue where the tab we were trying to update the icon for came back with an error that it did not exist, so doing a get of the tab and only executing the update of the icon if the tab with the tab id specified is found to exist
+    // chrome.tabs.get(getTabId(tab), function(tab) {
+    //   if (chrome.runtime.lastError) {
+    //       console.log(`chrome.runtime.lastError was thrown with message: ${chrome.runtime.lastError.message}`);
+    //   } else {
+    //     console.log(`chrome.runtime.lastError NOT detected, so going ahead and using the tab`);
+    //     console.log(`inside chrome.tabs.get async call to make sure the tab existed - it did and next we will call chrome.browserAction.setIcon yellow on tab ${JSON.stringify(tab)} using tab id determined to be: ${getTabId(tab)}`);
+    //     chrome.browserAction.setIcon({path: "ora_icon_off_yellow_128.png", tabId:getTabId(tab)});
+    //     console.log(`just did the chrome.browserAction.setIcon call to make the icon yellow, if we got here, should be showing yellow`);
+    //   }
+    // });
+
+    // chrome.tabs.query({
+    // "active":        true,
+    // "currentWindow": true,
+    // "status":        "complete",
+    // "windowType":    "normal"
+    // }, function(tab) {
+    //   if (chrome.runtime.lastError) {
+    //       console.log(`chrome.runtime.lastError was thrown with message: ${chrome.runtime.lastError.message}`);
+    //   } else {
+    //     console.log(`chrome.runtime.lastError NOT detected, so going ahead and using the tab`);
+    //     console.log(`inside chrome.tabs.get async call to make sure the tab existed - it did and next we will call chrome.browserAction.setIcon yellow on tab ${JSON.stringify(tab)} using tab id determined to be: ${getTabId(tab)}`);
+    //     chrome.browserAction.setIcon({path: "ora_icon_off_yellow_128.png"});
+    //     console.log(`just did the chrome.browserAction.setIcon call to make the icon yellow, if we got here, should be showing yellow`);
+    //   }
+    // });
+
   }
 
   /*
    * change the extension icon to dark to indicate we are on a non-KR page or it's been disabled
    */
-   function setExtensionIconDisabledColor(tab) {
-     chrome.browserAction.setIcon({path: "ora_icon_off_dark_128.png", tabId:getTabId(tab)});
+   function setExtensionIconDisabledColor() {
+     chrome.browserAction.setIcon({path: "ora_icon_off_dark_128.png"});
    }
 
 
@@ -205,3 +274,41 @@
        return undefined;
      }
    }
+
+
+
+   /* may need later
+    function setExtensionIconInactiveColor(tab) {
+      console.log(`inside setExtensionIconInactiveColor the tab passed in shows: ${JSON.stringify(tab)} and also the getTabId(tab) being used is showing: ${getTabId(tab)}`);
+      console.log(`about to set icon to yellow (inside setExtensionIconInactiveColor)`);
+      // we were running into issue where the tab we were trying to update the icon for came back with an error that it did not exist, so doing a get of the tab and only executing the update of the icon if the tab with the tab id specified is found to exist
+      // chrome.tabs.get(getTabId(tab), function(tab) {
+      //   if (chrome.runtime.lastError) {
+      //       console.log(`chrome.runtime.lastError was thrown with message: ${chrome.runtime.lastError.message}`);
+      //   } else {
+      //     console.log(`chrome.runtime.lastError NOT detected, so going ahead and using the tab`);
+      //     console.log(`inside chrome.tabs.get async call to make sure the tab existed - it did and next we will call chrome.browserAction.setIcon yellow on tab ${JSON.stringify(tab)} using tab id determined to be: ${getTabId(tab)}`);
+      //     chrome.browserAction.setIcon({path: "ora_icon_off_yellow_128.png", tabId:getTabId(tab)});
+      //     console.log(`just did the chrome.browserAction.setIcon call to make the icon yellow, if we got here, should be showing yellow`);
+      //   }
+      // });
+
+      chrome.tabs.query({
+      "active":        true,
+      "currentWindow": true,
+      "status":        "complete",
+      "windowType":    "normal"
+      }, function(tab) {
+        if (chrome.runtime.lastError) {
+            console.log(`chrome.runtime.lastError was thrown with message: ${chrome.runtime.lastError.message}`);
+        } else {
+          console.log(`chrome.runtime.lastError NOT detected, so going ahead and using the tab`);
+          console.log(`inside chrome.tabs.get async call to make sure the tab existed - it did and next we will call chrome.browserAction.setIcon yellow on tab ${JSON.stringify(tab)} using tab id determined to be: ${getTabId(tab)}`);
+          chrome.browserAction.setIcon({path: "ora_icon_off_yellow_128.png", tabId:getTabId(tab)});
+          console.log(`just did the chrome.browserAction.setIcon call to make the icon yellow, if we got here, should be showing yellow`);
+        }
+      });
+
+    }
+
+    */
